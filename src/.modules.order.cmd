7f61613b81932def0e9b4ec904570d6a96e17b2b
0xwillow <0xwillow@protonmail.com>
2023-08-22 18:40:38 -0700

    remove junk files

--- src/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/Desktop/projects/kvmrk_arm/src/modules.order := {   echo /mnt/Desktop/projects/kvmrk_arm/src/kvmrk.ko; :; } | awk '!x[$$0]++' - > /mnt/Desktop/projects/kvmrk_arm/src/modules.order
